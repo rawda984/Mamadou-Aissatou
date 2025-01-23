7a863a0e2511173eafb20f1a5ed7197550c3bd72
rawda984 <rawdamamadouaissatou1@gmail.com>
2025-01-23 09:20:03 +0100

    commit

---
+++ Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SampleLib.h"
+int main( void ) {
+ int result = power( 2, 3 );
+ printf( "2³ == %d\n", result );
+ result = fact( 6 );
+ printf( "6! == %d\n", result );
+ return EXIT_SUCCESS; 
+ }
